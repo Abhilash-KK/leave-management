fdfa21be57b3f874619522907b8fffa00607cbab
Abhilash <kkabhilash30@gmail.com>
2026-05-06 21:43:55 +0530

    added mysql support

--- leave_project/__init__.py
+++ leave_project/__init__.py
@@ -1,1 +1,10 @@
-# Leave Management Project
+# Monkey patch to bypass Django's version check for MariaDB/MySQL
+try:
+    from django.db.backends.mysql.base import DatabaseWrapper
+    DatabaseWrapper.check_database_version_supported = lambda self: None
+
+    from django.db.backends.mysql.features import DatabaseFeatures
+    DatabaseFeatures.can_return_columns_from_insert = False
+    DatabaseFeatures.can_return_rows_from_bulk_insert = False
+except ImportError:
+    pass
