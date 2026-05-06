66bc0d304f5352967d07775b6e0f698e67d5b0ab
Abhilash <kkabhilash30@gmail.com>
2026-05-06 21:23:09 +0530

    requirement updated

--- leave_project/__init__.py
+++ leave_project/__init__.py
@@ -1,11 +1,1 @@
-# Monkey patch to bypass Django's version check for MariaDB/MySQL because XAMPP uses MariaDB 10.4
-# Also disable RETURNING features because MariaDB < 10.5 does not support it.
-try:
-    from django.db.backends.mysql.base import DatabaseWrapper
-    DatabaseWrapper.check_database_version_supported = lambda self: None
-
-    from django.db.backends.mysql.features import DatabaseFeatures
-    DatabaseFeatures.can_return_columns_from_insert = False
-    DatabaseFeatures.can_return_rows_from_bulk_insert = False
-except ImportError:
-    pass
+# Leave Management Project
